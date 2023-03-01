b17d59a04d65ce38a25bfcdf55e1dd38b8663757
huanvu92 <burberryvu@gmail.com>
2023-02-28 19:48:08 -0800

    Create my_list = [-23, 3, 2, 890, 43, -50, 100].py

---
+++ my_list = [-23, 3, 2, 890, 43, -50, 100].py
@@ -0,0 +1,23 @@
+my_list = [-23, 3, 2, 890, 43, -50, 100]
+print(my_list)
+
+my_list = [-23, 3, 2, 890, 43, -50, 100]
+print(my_list[3])
+
+uclaCollege = []
+
+uclaCollege.append("University of California at Los Angeles")
+uclaCollege.append(43201)
+uclaCollege.append("State College")
+uclaCollege.append(True)
+
+print(uclaCollege)
+
+sjccCollege = {}
+
+sjccCollege["name"] = "San Jose City College"
+sjccCollege["students"] = 8584
+sjccCollege["type"] = "Community College"
+sjccCollege["hasSports"] = True
+
+print(sjccCollege)
